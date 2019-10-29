7c3d0c5c938f68e086982e1c69533d0078bdd65b
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-10-29 18:11:02 -0400

    verbose logging of registration

--- src/endpoints/slash-toots.lisp
+++ src/endpoints/slash-toots.lisp
@@ -49,51 +49,58 @@
   "Create a new Toot.
 
 Input JSON must have the following fields:
-     tootName, baseColor, padColor, pattern, patternColor, tShirtColor
+     name, baseColor, padColor, pattern, patternColor, tShirtColor
 
 Responds with 201 (Created); or 409 (Conflict)  if the name is in use or
 for some other reason the value can't  be entered; 422 if the Toot name,
 color  or pattern  name(s)  given are  not valid.  (400  if the  request
 is malformed.)"
   (with-user ()
-    (with-posted-json (Toot-name base-color pad-color pattern pattern-color
-                                 t-shirt-color)
+    (with-posted-json (name base-color pad-color pattern pattern-color
+                            t-shirt-color)
+      (v:info :registration
+              "Begin registration request for ~:(~a~)" name)
       (with-errors-as-http (422)
-        (check-type Toot-name Toot-name)
+        (check-type name Toot-name)
         (check-type base-color Toot-base-color-name)
         (check-type pad-color Toot-pad-color-name)
         (check-type pattern Toot-pattern-name)
         (check-type pattern-color Toot-pattern-color-name)
         (assert (member t-shirt-color +initial-t-shirt-colors+
-                        :test 'string-equal)))
+                        :test 'string-equal))
+        (v:info :registration
+                "Registration request for ~:(~a~) seems sane" name))
       (with-errors-as-http (409)
-        (assert (not (find-record 'Toot :name Toot-name)))
+        (assert (not (ignore-not-found (find-record 'Toot :name name))))
         (let ((Toot
-                (make-record 'Toot
-                             :name toot-name
-                             :player (person-uuid *user*)
-                             :pattern (pattern-id (find-record 'pattern
-                                                               :name pattern))
-                             :base-color (parse-color24 base-color)
-                             :pattern-color (parse-color24 pattern-color)
-                             :pad-color (parse-color24 pad-color)
-                             :player (person-uuid *user*)
-                             :avatar-scale-x 1.0
-                             :avatar-scale-y 1.0
-                             :avatar-scale-z 1.0
-                             :avatar 1   ; UltraToot
-                             :note (concatenate
-                                    'string
-                                    "New Toot registered via web from "
-                                    (hunchentoot:remote-addr*))))
+               (prog1
+                   (make-record 'Toot
+                                :name name
+                                :player (person-uuid *user*)
+                                :pattern (pattern-id (find-record 'pattern
+                                                                  :name pattern))
+                                :base-color (parse-color24 base-color)
+                                :pattern-color (parse-color24 pattern-color)
+                                :pad-color (parse-color24 pad-color)
+                                :player (person-uuid *user*)
+                                :avatar-scale-x 1.0
+                                :avatar-scale-y 1.0
+                                :avatar-scale-z 1.0
+                                :avatar 1  ; UltraToot
+                                :note (concatenate
+                                       'string
+                                       "New Toot registered via web from "
+                                       (hunchentoot:remote-addr*)))
+                 (v:info :registration
+                         "Created new Toot ~:(~a~)" name)))
               (t-shirt
-                (make-record 'item
-                             :base-color (parse-color24 t-shirt-color)
-                             :alt-color (color24-rgb 0 0 0)
-                             :template 1       ; Solid color basic T-shirt
-                             :avatar-scale-x 1.0
-                             :avatar-scale-y 1.0
-                             :avatar-scale-z 1.0)))
+               (make-record 'item
+                            :base-color (parse-color24 t-shirt-color)
+                            :alt-color (color24-rgb 0 0 0)
+                            :template 1       ; Solid color basic T-shirt
+                            :avatar-scale-x 1.0
+                            :avatar-scale-y 1.0
+                            :avatar-scale-z 1.0)))
           (save-record Toot)
           (save-record t-shirt)
           (save-record
