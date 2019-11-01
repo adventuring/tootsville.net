b5c8673a896e08a4da3fdf1e309950a33e757f89
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-11-01 12:50:33 -0400

    remove verbose logging

--- src/web.lisp
+++ src/web.lisp
@@ -47,7 +47,7 @@ the request URI for \".js\" (which  is, of course, a subseq of \".json\"
 as well.)"
   (let ((result (or (search "application/json" (the string (hunchentoot:header-in* :accept)))
                     (search ".js" (the string (hunchentoot:request-uri*))))))
-    (v:info :wants-json-p "Wants-JSON-P? Decided that we ~:[don't~;do~] want JSON" result)
+    #+ (or) (v:info :wants-json-p "Wants-JSON-P? Decided that we ~:[don't~;do~] want JSON" result)
     result))
 
 
