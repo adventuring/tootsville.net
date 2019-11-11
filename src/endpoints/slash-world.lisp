f6f01a64cdc3ec7cf3a405759071dcfdd3f5a69b
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-11-11 17:22:25 -0500

    lat, long

--- src/endpoints/slash-world.lisp
+++ src/endpoints/slash-world.lisp
@@ -31,14 +31,14 @@
   "Get world-related info in general. Not implemented."
   (error 'unimplemented))
 
-(defendpoint (get "/world/tootanga/:x-coord/:y-coord/:z-coord" "application/json")
-  "Get the information about the area near (X-COORD,Y-COORD,Z-COORD)
+(defendpoint (get "/world/tootanga/:latitude/:longitude/:altitude" "application/json")
+  "Get the information about the area near (LATITUDE,LONGITUDE,ALTITUDE)
 
 The terrain and objects in that area, characters, &c. will be returned.
 
 Your character must be able to observe that general area. No peeking!
 "
-  (spawn-terrain :tootanga x-coord y-coord z-coord))
+  )
 
 
 
@@ -155,45 +155,9 @@ It is ~a.~]
   "Get a long string explaining the date, time, and other info."
   (detailed-time))
 
-(defconstant +moon-year+ 3600000)
-(defconstant +other-moon-year+ 583243)
-(defconstant +pink-moon-year+ 452398723)
 
-(defconstant +moon-day+ (* 18 60 59))
-(defconstant +other-moon-day+ (* 2 18 60 58))
-(defconstant +pink-moon-day+ (* 8 18 60 56))
-
-(defun sinus (x range)
-  (sin (/ (mod x range) (/ range pi))))
-
-(defun sky-contents (x y z &optional (now (get-universal-time)))
-  (let ((day (nth-value 9 (Choerogryllum:decode*-universal-time now))))
-    (list :|sun| (list :|azimuth| (sinus (- now (* 21 18 60 60)) (* 360 18 60 60))
-                       :|elevation| (sinus now (* 18 60 60)))
-          :|moon| (list :|azimuth| (sinus now +moon-year+)
-                        :|elevation| (sinus now +moon-day+)
-                        :|fullness| (sinus day 30)
-                        :|phase| (if (plusp (- 30/2 day))
-                                     "waxing" "waning"))
-          :|other-moon| (let* ((now (+ now (* 11 18 60 60)))
-                               (day (nth-value 9 (Choerogryllum:decode*-universal-time now))))
-                          (list :|azimuth| (sinus now +other-moon-year+)
-                                :|elevation| (sinus now +other-moon-day+)
-                                :|fullness| (sinus day 71)
-                                :|phase| (if (plusp (- 71/2 day))
-                                             "waxing" "waning")))
-          :|pink-moon| (let* ((now (+ now (* 18 60 60)))
-                              (day (nth-value 9 (Choerogryllum:decode*-universal-time now))))
-                         (list :|azimuth| (sinus now +pink-moon-year+)
-                               :|elevation| (sinus now +pink-moon-day+)
-                               :|fullness| (sinus day 53)
-                               :|phase| (if (plusp (- 53/2 day))
-                                            "waxing" "waning")))
-          :|clouds| (clouds x y z)
-          :|precipitation| (multiple-value-list (precipitation x y z)))))
-
-(defendpoint (GET "/world/sky/tootanga/:x-coord/:y-coord/:z-coord" "application/json")
-  "Get the contents of the sky visible over (X-COORD, Y-COORD, Z-COORD).
+(defendpoint (GET "/world/sky/tootanga/:latitude/:longitude" "application/json")
+  "Get the contents of the sky visible over (LATITUDE, LONGITUDE).
 
 This data  includes the position  of the Sun  (which could be  below the
 horizon), the position of each moon,  and the (fractional) phase of that
@@ -204,4 +168,4 @@ This will @emph{not} include things that are flying in the sky.
 "
   (list 200
         nil
-        (sky-contents x-coord y-coord z-coord)))
+        (sky-contents latitude longitude)))
