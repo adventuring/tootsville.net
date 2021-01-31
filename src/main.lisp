5220845940512d76daf6ad3f7e50f39b23c97118
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-01-31 15:12:42 -0500

    re-introduce friendly hack

--- src/main.lisp
+++ src/main.lisp
@@ -150,8 +150,8 @@ is available."
     (cassandra-boot)
     (background-gc))
 
-  ;; (v:info '(:starting :hack) "HACK … reloading friendly neighborhood database definition file …")
-  ;; (load (asdf:system-relative-pathname :Tootsville "src/db/friendly.lisp"))
+  (v:info '(:starting :hack) "HACK … reloading friendly neighborhood database definition file …")
+  (load (asdf:system-relative-pathname :Tootsville "src/db/friendly.lisp"))
   
   (setf hunchentoot:*hunchentoot-default-external-format* :utf-8)
 
