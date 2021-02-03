b2c79e7570047d60f70ee37817a3e846c40d3bce
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-02-03 14:47:11 -0500

    migrations.sql

--- src/db/maria.lisp
+++ src/db/maria.lisp
@@ -349,39 +349,12 @@ LOCK-NAME is case-insensitive."
 
 
 
-(defvar *migrations*
-  '("create table if not exists 
-cassandra_blacklist ( pattern varchar(255), constraint pattern_key primary key (pattern) )
-engine=InnoDB default charset=utf8;"
-    "create table if not exists
-cassandra_redlist ( pattern varchar(255), constraint pattern_key primary key (pattern) )
-engine=InnoDB default charset=utf8;"
-    "create table if not exists
-staff_journal_entries 
-( uuid char(22) not null,
-  written_by char(22) not null,
-  written_at datetime not null,
-  entry text,
-  constraint uuid_key primary key (uuid),
-   constraint written_by_person foreign key (written_by) references people (uuid) on delete restrict on update cascade )
-engine=InnoDB default charset=utf8;"
-    "create table if not exists
-staff_journal_references
-( entry char(22) not null,
-  person char(22) not null,
-  constraint pair_key primary key (entry, person),
-  constraint staff_journal_entry foreign key (entry) references staff_journal_entries (uuid) on delete restrict on update cascade,
-  constraint about_whom foreign key (person) references people (uuid) on delete restrict on update cascade )
-engine=InnoDB default charset=utf8;"
-    "alter table items add column if not exists effect varchar(64);"
-    "alter table items add column if not exists attributes text;"
-    "insert ignore into item_templates (id, name, default_base_color, avatar, wear_slot, description) values (6, 'Snowball', 'FFFFFF', 'snowball', 12, 'A ball of snow that makes
-a playful missile.');"))
-
 (defun perform-all-migrations ()
   "Perform all necessary database migrations."
   
-  (dolist (migration *migrations*)
-    (with-dbi (:friendly) 
-      (format t "Migration: ~a ~{~2%~{~%~a: ~a~}~}" migration
+  (with-dbi (:friendly) 
+    (dolist (migration (split-sequence #\Page
+                                       (read-file-into-string (asdf:system-relative-pathname (asdf:find-system :Tootsville)
+                                                                                             #p "src/db/migrations.sql"))))
+      (format t "~2&Migration: ~a ~{~2%~{~%~a: ~a~}~}" migration
               (db-select-all *db* migration)))))
